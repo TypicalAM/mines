0ac02f599c187bdac5a432b7f901e405884f67f5
Adam Piaseczny <cypis183@gmail.com>
2022-11-05 04:25:05 +0100

    Added difficulty presets

--- src/screens/options/options.go
+++ src/screens/options/options.go
@@ -2,7 +2,7 @@ package options
 
 import (
 	"example/raylib-game/src/gui"
-	"example/raylib-game/src/screens"
+	shared "example/raylib-game/src/screens"
 	"example/raylib-game/src/settings"
 	"fmt"
 	"strconv"
@@ -22,14 +22,32 @@ const (
 
 // Option structure
 type option struct {
-	optionRectangle rl.Rectangle
-	textRectangle   rl.Rectangle
-	value           string
-	valType         int
+	bounds     rl.Rectangle
+	textBounds rl.Rectangle
+	value      string
+	valType    int
 }
 
-// Variable for all our options
+// A map for all our options with described key names
+const keyWidth string = "Width of the map"
+const keyHeight string = "Height of the map"
+const keyBombs string = "Bombs count percentage"
+const keySettingsPath string = "Path to the settings file"
+const keyColorscheme string = "Color scheme"
+
 var options map[string]option
+
+// Make the preset variables
+type preset struct {
+	bounds    rl.Rectangle
+	isPressed bool
+	name      string
+	width     int
+	height    int
+	bombs     int
+}
+
+var presets [3]preset
 
 // Can we save?
 var saveAndExit bool
@@ -38,7 +56,7 @@ var saveOptionsRect rl.Rectangle
 
 // What theme are we using?
 var activeThemeIndex int
-var themes = []string{"default_dark", "default_light", "candy","hello_kitty", "monokai", "obsidian", "solarized", "solarized_light", "zahnrad"}
+var themes = []string{"default_dark", "default_light", "candy", "hello_kitty", "monokai", "obsidian", "solarized", "solarized_light", "zahnrad"}
 
 // Options screen initialization logic
 func Init() {
@@ -53,36 +71,63 @@ func Init() {
 	baseTextY := -275
 	baseOffsetY := 100
 
+	// Create the preset rectangles
+	presets[0] = preset{
+		bounds:    rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY-baseOffsetY), rectangleWidths/3, 60),
+		isPressed: false,
+		name:      "Beginner",
+		width:     8,
+		height:    8,
+		bombs:     15,
+	}
+	presets[1] = preset{
+		bounds:    rl.NewRectangle(rectangleXPos+float32(1*int(rectangleWidths)/3), float32(rl.GetScreenHeight()/2+baseRectY-baseOffsetY), rectangleWidths/3, 60),
+		isPressed: false,
+		name:      "Intermediate",
+		width:     16,
+		height:    16,
+		bombs:     15,
+	}
+	presets[2] = preset{
+		bounds:    rl.NewRectangle(rectangleXPos+float32(2*int(rectangleWidths)/3), float32(rl.GetScreenHeight()/2+baseRectY-baseOffsetY), rectangleWidths/3, 60),
+		isPressed: false,
+		name:      "Advanced",
+		width:     30,
+		height:    16,
+		bombs:     21,
+	}
+
+	// Create the options
 	options = map[string]option{
-		"Width of the map": {
-			optionRectangle: rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY), rectangleWidths, 60),
-			textRectangle:   rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Width of the map - 100", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY), rectangleWidths, 60),
-			value:           fmt.Sprint(shared.AppSettings.Width),
-			valType:         Slider,
+		keyWidth: {
+			bounds:     rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY), rectangleWidths, 60),
+			textBounds: rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Width of the map - 100", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY), rectangleWidths, 60),
+			value:      fmt.Sprint(shared.AppSettings.Width),
+			valType:    Slider,
 		},
-		"Height of the map": {
-			optionRectangle: rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+baseOffsetY), rectangleWidths, 60),
-			textRectangle:   rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Height of the map - 100", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+baseOffsetY), rectangleWidths, 60),
-			value:           fmt.Sprint(shared.AppSettings.Height),
-			valType:         Slider,
+		keyHeight: {
+			bounds:     rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+baseOffsetY), rectangleWidths, 60),
+			textBounds: rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Height of the map - 100", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+baseOffsetY), rectangleWidths, 60),
+			value:      fmt.Sprint(shared.AppSettings.Height),
+			valType:    Slider,
 		},
-		"Bombs count percentage": {
-			optionRectangle: rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+2*baseOffsetY), rectangleWidths, 60),
-			textRectangle:   rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Bombs count percentage - 100", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+2*baseOffsetY), rectangleWidths, 60),
-			value:           fmt.Sprint(shared.AppSettings.Bombs),
-			valType:         Slider,
+		keyBombs: {
+			bounds:     rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+2*baseOffsetY), rectangleWidths, 60),
+			textBounds: rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Bombs count percentage - 100", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+2*baseOffsetY), rectangleWidths, 60),
+			value:      fmt.Sprint(shared.AppSettings.Bombs),
+			valType:    Slider,
 		},
 		"Path to the settings file": {
-			value:           shared.AppSettings.SettingsPath,
-			optionRectangle: rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+3*baseOffsetY), rectangleWidths, 60),
-			textRectangle:   rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Path to the settings file", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+3*baseOffsetY), rectangleWidths, 60),
-			valType:         FilePicker,
+			value:      shared.AppSettings.SettingsPath,
+			bounds:     rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+3*baseOffsetY), rectangleWidths, 60),
+			textBounds: rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Path to the settings file", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+3*baseOffsetY), rectangleWidths, 60),
+			valType:    FilePicker,
 		},
 		"Color scheme": {
-			value:           shared.AppSettings.Theme,
-			optionRectangle: rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+4*baseOffsetY), rectangleWidths, 60),
-			textRectangle:   rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Color scheme", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+4*baseOffsetY), rectangleWidths, 60),
-			valType:         Combo,
+			value:      shared.AppSettings.Theme,
+			bounds:     rl.NewRectangle(rectangleXPos, float32(rl.GetScreenHeight()/2+baseRectY+4*baseOffsetY), rectangleWidths, 60),
+			textBounds: rl.NewRectangle(rectangleXPos+rectangleWidths/2-rl.MeasureTextEx(shared.Font, "Color scheme", shared.FontSmallTextSize, 0).X/2, float32(rl.GetScreenHeight()/2+baseTextY+4*baseOffsetY), rectangleWidths, 60),
+			valType:    Combo,
 		},
 	}
 
@@ -103,10 +148,27 @@ func Init() {
 
 // Update Options screen
 func Update() {
+	// Go back to title
 	if rl.IsKeyPressed(rl.KeyEscape) {
 		ScreenState = shared.Title
 	}
 
+	// Update the sliders if the preset was pressed
+	for _, preset := range presets {
+		if preset.isPressed {
+			widthEntry := options[keyWidth]
+			widthEntry.value = fmt.Sprint(preset.width)
+			options[keyWidth] = widthEntry
+			heightEntry := options[keyHeight]
+			heightEntry.value = fmt.Sprint(preset.height)
+			options[keyHeight] = heightEntry
+			bombsEntry := options[keyBombs]
+			bombsEntry.value = fmt.Sprint(preset.bombs)
+			options[keyBombs] = bombsEntry
+		}
+	}
+
+	// Save results if we pressed the save results button
 	if saveAndExit {
 		saveResults()
 	}
@@ -119,6 +181,11 @@ func Draw() {
 	// Draw the logo
 	gui.DrawLogoTopLeft(shared.LogoIcon, shared.SecondaryFont, shared.IconRect, shared.TextRect, shared.FontHugeTextSize)
 
+	// Draw the preset rectangles
+	for pos, preset := range presets {
+		presets[pos].isPressed = gui.ButtonEx(shared.Font, preset.bounds, preset.name, shared.FontMediumTextSize)
+	}
+
 	for key, opt := range options {
 		var textToDisplay string
 
@@ -126,20 +193,20 @@ func Draw() {
 		case Slider:
 			currentValue, _ := strconv.Atoi(opt.value)
 			textToDisplay = fmt.Sprintf("%s : %d", key, currentValue)
-			opt.value = fmt.Sprintf("%d", int(rg.Slider(opt.optionRectangle, float32(currentValue), 1, 100)))
+			opt.value = fmt.Sprintf("%d", int(rg.Slider(opt.bounds, float32(currentValue), 1, 100)))
 		case FilePicker:
 			textToDisplay = fmt.Sprint(key)
-			opt.value = gui.FilePicker(shared.LogoIcon, shared.Font, opt.optionRectangle, opt.value, shared.FontBigTextSize)
+			opt.value = gui.FilePicker(shared.LogoIcon, shared.Font, opt.bounds, opt.value, shared.FontBigTextSize)
 		case Combo:
 			textToDisplay = fmt.Sprint(key)
-			activeThemeIndex = gui.ComboBoxEx(shared.Font, opt.optionRectangle, themes, activeThemeIndex, shared.FontBigTextSize)
+			activeThemeIndex = gui.ComboBoxEx(shared.Font, opt.bounds, themes, activeThemeIndex, shared.FontBigTextSize)
 			opt.value = fmt.Sprint(themes[activeThemeIndex])
 			rg.LoadGuiStyle(fmt.Sprintf("resources/styles/%s.style", opt.value))
 		}
 
 		options[key] = opt
 
-		rl.DrawTextEx(shared.Font, textToDisplay, rl.Vector2{X: opt.textRectangle.X, Y: opt.textRectangle.Y}, shared.FontSmallTextSize, 0, rg.GetStyleColor(rg.TextboxTextColor))
+		rl.DrawTextEx(shared.Font, textToDisplay, rl.Vector2{X: opt.textBounds.X, Y: opt.textBounds.Y}, shared.FontSmallTextSize, 0, rg.GetStyleColor(rg.TextboxTextColor))
 	}
 
 	saveAndExit = gui.ButtonEx(shared.Font, saveOptionsRect, "SAVE", shared.FontBigTextSize)
@@ -150,11 +217,11 @@ func Draw() {
 
 // Get all the fields and save it
 func saveResults() {
-	width, _ := strconv.Atoi(options["Width of the map"].value)
-	height, _ := strconv.Atoi(options["Height of the map"].value)
-	bombs, _ := strconv.Atoi(options["Bombs count percentage"].value)
-	path := options["Path to the settings file"].value
-	theme := options["Color scheme"].value
+	width, _ := strconv.Atoi(options[keyWidth].value)
+	height, _ := strconv.Atoi(options[keyHeight].value)
+	bombs, _ := strconv.Atoi(options[keyBombs].value)
+	path := options[keySettingsPath].value
+	theme := options[keyColorscheme].value
 
 	newSettings := settings.Settings{
 		Width:        width,
--- todo.md
+++ todo.md
@@ -1,9 +1,17 @@
 Game winning logic:
-- Calculatign the boards' CPM (or what was it called)
-- Tests for inserting new scores
+	- Calculatign the boards' CPM (or what was it called)
+	- Tests for inserting new scores
 
-Tab navigation for the options screen
-Fix title big res
-The restart button on the gameplay screen (maybe a fancy animation)
-Make loading gifs instead of frames
-Window icon?
+Options screen:
+	 - Tab navigation
+
+Gameplay screen:
+	- The restart button on the gameplay screen (maybe a fancy animation)
+	- Make loading gifs instead of frames
+	- Make the first click be not a bomb (move it to a different place)
+
+Scoreboard screen:
+	- Make the scores for different categories
+
+General:
+	- Window icon?
